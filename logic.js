6840b4207a328cbd7acb7ac5a926f256102ecf33
tsaifoon <etsai1987@gmail.com>
2019-05-18 22:21:53 -0400

    first commit

---
+++ logic.js
@@ -0,0 +1,92 @@
+// Create a map object
+var myMap = L.map("map", {
+  center: [15.5994, -28.6731],
+  zoom: 3
+});
+
+// Adding tile layer
+L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+  attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+  maxZoom: 18,
+  id: "mapbox.streets-basic",
+  accessToken: "pk.eyJ1IjoidHNhaWZvb24iLCJhIjoiY2p2ZWN1ZXhjMmk0czQ0bWVkanRtN2tvYSJ9.LUMf3ZCyC40rtmofO5kAJw"
+}).addTo(myMap);
+
+function markerSize(population) {
+  return population * 1000;
+}
+
+// Country data
+var countries = [
+  {
+    name: "Brazil",
+    location: [-14.2350, -51.9253],
+    points: 227
+  },
+  {
+    name: "Germany",
+    location: [51.1657, 10.4515],
+    points: 218
+  },
+  {
+    name: "Italy",
+    location: [41.8719, 12.5675],
+    points: 156
+  },
+  {
+    name: "Argentina",
+    location: [-38.4161, -63.6167],
+    points: 140
+  },
+  {
+    name: "Spain",
+    location: [40.4637, -3.7492],
+    points: 99
+  },
+  {
+    name: "England",
+    location: [52.355, 1.1743],
+    points: 98
+  },
+  {
+    name: "France",
+    location: [46.2276, 2.2137],
+    points: 96
+  },
+  {
+    name: "Netherlands",
+    location: [52.1326, 5.2913],
+    points: 93
+  },
+  {
+    name: "Uruguay",
+    location: [-32.4228, -55.7658],
+    points: 72
+  },
+  {
+    name: "Sweden",
+    location: [60.1282, 18.6435],
+    points: 61
+  }
+];
+
+
+// Loop through the countries array
+
+  // Conditionals for countries points
+
+  // Add circles to map
+
+
+  // Adjust radius
+// Loop through the cities array and create one marker for each city object
+for (var i = 0; i < countries.length; i++) {
+  L.circle(countries[i].location, {
+    fillOpacity: 0.75,
+    color: "white",
+    fillColor: "blue",
+    // Setting our circle's radius equal to the output of our markerSize function
+    // This will make our marker's size proportionate to its population
+    radius: markerSize(countries[i].points)
+  }).bindPopup("<h1>" + countries[i].name + "</h1> <hr> <h3>Points: " + countries[i].points + "</h3>").addTo(myMap);
+}
---
+++ logic2.js
@@ -0,0 +1,75 @@
+//link
+var queryUrl = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson";
+
+// Perform a GET request to the query URL
+d3.json(queryUrl, function(data) {
+  // Once we get a response, send the data.features object to the createFeatures function
+  createFeatures(data.features);
+});
+
+var eqMag = []
+
+function createFeatures(earthquakeData) {    
+
+    // Define a function we want to run once for each feature in the features array
+    // Give each feature a popup describing the place and time of the earthquake
+    function onEachFeature(feature, layer) {
+      layer.bindPopup("<h1>" + feature.properties.title + "</h1> <hr> <h3>Place: " + feature.properties.place + "</h3> <br> <h3> Magnitude: " + feature.properties.mag + "</h3>");
+    }
+  
+    // Create a GeoJSON layer containing the features array on the earthquakeData object
+    // Run the onEachFeature function once for each piece of data in the array
+    var earthquakes = L.geoJSON(earthquakeData, {
+      pointToLayer: function(feature,latlng){
+        return new L.Circle(latlng,
+          {    
+          fillOpacity: 0.75,
+          color: "white",
+          fillColor: "blue",
+          radius: feature.properties.mag * 100
+          });
+        },
+      onEachFeature: onEachFeature
+    });
+  
+    // Sending our earthquakes layer to the createMap function
+    createMap(earthquakes);
+  }
+
+  function createMap(earthquakes) {
+
+    // Define streetmap and darkmap layers
+    var streetmap = L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
+      attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
+      maxZoom: 18,
+      id: "mapbox.streets",
+      accessToken: API_KEY
+    });
+  
+    // Define a baseMaps object to hold our base layers
+    var baseMaps = {
+      "Street Map": streetmap,
+    };
+    
+    // Create overlay object to hold our overlay layer
+    var overlayMaps = {
+      Earthquakes: earthquakes,
+    };
+  
+    // Create our map, giving it the streetmap and earthquakes layers to display on load
+    var myMap = L.map("map", {
+      center: [
+        37.09, -95.71
+      ],
+      zoom: 5,
+      layers: [streetmap, earthquakes]
+    });
+  
+    // Create a layer control
+    // Pass in our baseMaps and overlayMaps
+    // Add the layer control to the map
+    L.control.layers(baseMaps, overlayMaps, {
+      collapsed: false
+    }).addTo(myMap);
+  
+  }
